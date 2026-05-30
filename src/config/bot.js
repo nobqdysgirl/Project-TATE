5b0ef211c9b1a275330df39a24f6fc5b26aaa1e4
nobqdysgirl <oradv8dancecomplex@gmail.com>
2026-05-30 14:35:50 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
-        // Activity type number (0 = Playing).
-        type: 0, 
+        name: "Project TATE runs",
+        // Activity type number (3 = Playing).
+        type: 3, Project TATE
       },
     ],
   },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#ff00ec", 
+      secondary: "#000000", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
